4edb0c43235c4b4ef4367b6da9185910568959a6
marianorigo18 <marianowagnerrigo@gmail.com>
2023-04-03 17:10:36 -0300

    fix commit

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,15 +3,42 @@ import { getCategories } from "../../asyncMocks"
 
 const ItemListContainer = () => {
     const [products, setProducts] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(() => {
         getCategories()
         .then(response => {
             setProducts(response)
         })
-    })
-    console.log(products);
+        .catch(err =>{
+            console.log(err)
+        })
+        .finally(()=>{
+            setIsLoading(false)
+        })
+    },[])
+
+    if(isLoading){
+        return <h1>cargando...</h1>
+    }
     return(
-        <h1>Hello world from ItemListContainer</h1>
+        <div>
+            <h1>Hello world from ItemListContainer</h1>
+            <div style={{display: "flex", flexWrap: "wrap"}}>
+                {products.map((prod) => {
+                    return(
+                        prod.categorias.map((prod2, index)=>{
+                            return(
+                                <div key={index} style={{border: "1px solid black"}}>
+                                    <h1>{prod2.title}</h1>
+                                    <img src={prod2.img} alt={prod2.title} width={100}></img>
+                                </div>
+                            )
+                        })
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 export default ItemListContainer
