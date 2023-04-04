8f4deec2b14a41cf1f50e6345d578bb954cd552b
marianorigo18 <marianowagnerrigo@gmail.com>
2023-04-04 17:55:53 -0300

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { getCategories } from "./asyncMocks";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   getCategories()
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ItemListContainer/>}/>
         <Route path="/category/:categoryId" element={<ItemListContainer/>}/>
+        <Route path="/item/:productId" element={<ItemDetailContainer/>}/>
       </Routes>
     </BrowserRouter>
     );
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,9 @@
+const ItemDetailContainer = () => {
+    return(
+        <div>
+            <h1>Detail</h1>
+        </div>
+    )
+}
+
+export default ItemDetailContainer
